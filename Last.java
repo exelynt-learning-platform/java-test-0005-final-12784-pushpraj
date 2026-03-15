927a2586616204cb16cfe55f22c7020e745c1bcb
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-16 03:54:21 +0530

    Update Last.java

--- Last.java
+++ Last.java
@@ -2,7 +2,7 @@ public class Last {
     public static void main(String[] args) {
         final int PYRAMID_HEIGHT = 5;
         int count = 1;
-        for(int row = 1; row <= PYRAMID_HEIGHT; row++) {
+        for(int i = 1; i <= PYRAMID_HEIGHT; i++) {
             for(int j = 1; j <= i; j++) {
                 System.out.print(count++ + " ");
             }
